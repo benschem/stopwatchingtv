29724856dc23364e1dfb5d8c1b49a1069453b28d
Ben Schembri <benschembri@gmail.com>
2025-12-27 18:42:13 +1100

    squash: into feat: routes and controller actions...

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'activities#index'
 
   resources :activities, only: %i[index create destroy] do
     collection do
